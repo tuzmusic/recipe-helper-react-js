a0e8ccbe4ed52140f68a824f2b00ca407bf76e07
tuzmusic <tuzmusic@gmail.com>
2019-07-08 18:06:28 -0400

    everything works again

--- __tests__/redux/recipesReducer.test.js
+++ __tests__/redux/recipesReducer.test.js
@@ -3,5 +3,7 @@ import recipesReducer from "../../src/redux/reducers/recipesReducer";
 
 describe("recipe reducer", () => {
   const initialState = { recipes: [] };
-  it("should ", () => {});
+  it("should return an initial state", () => {
+    expect(recipesReducer(initialState, {})).toEqual(initialState);
+  });
 });
