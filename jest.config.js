16e866bb7c00d60876221afb737e780d4a7311e7
tuzmusic <tuzmusic@gmail.com>
2019-07-08 16:55:03 -0400

    add config files

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: true,
+  setupFilesAfterEnv: ["<rootDir>/setupTest.js"],
+  transform: {
+    "^.+\\.js$": "babel-jest"
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  }
+};
