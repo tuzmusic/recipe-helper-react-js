0cbb6a75ddca73793b0233e75c830ac8a2886210
tuzmusic <tuzmusic@gmail.com>
2019-07-15 17:36:20 -0400

    implement CREATE_RECIPE_SUCCESS in reducer

--- __tests__/screens/NewRecipePage.test.js
+++ __tests__/screens/NewRecipePage.test.js
@@ -2,18 +2,23 @@ import React from "react";
 import { render, fireEvent, route } from "../../test-utils";
 import NewRecipePage from "../../src/screens/NewRecipePage/NewRecipePage";
 
-fdescribe("New Recipe Page", () => {
-  function inputCalled(name) {
-    return `<input label="${name}"`;
-  }
-  const $ = document.querySelector;
-  it("has a recipe form", () => {
-    const { getByText, getByLabelText } = route(<NewRecipePage />);
-    expect(getByLabelText("Title")).toBeDefined();
-    expect(getByLabelText("Ingredients")).toBeDefined();
-    expect(getByLabelText("Instructions")).toBeDefined();
+describe("New Recipe Page", () => {
+  let $;
+  beforeEach(() => {
+    $ = route(<NewRecipePage />);
   });
-  it("has a save button", () => {
-    expect(getByText("Save Recipe")).toBeDefined();
+  describe("the page", () => {
+    it("has a recipe form", () => {
+      expect($.getByLabelText("Title")).toBeDefined();
+      expect($.getByLabelText("Ingredients")).toBeDefined();
+      expect($.getByLabelText("Instructions")).toBeDefined();
+    });
+    it("has a save button", () => {
+      expect($.getByText("Save Recipe")).toBeDefined();
+    });
+  });
+
+  describe("operation", () => {
+    it("creates a recipe", () => {});
   });
 });
--- src/redux/reducers/recipesReducer.js
+++ src/redux/reducers/recipesReducer.js
@@ -5,9 +5,10 @@ import mockRecipes from "../../api-data/recipes";
 type State = { recipes: Recipe[] };
 
 type RecipeAction =
-  | { type: "GET_RECIPES_START" }
+  | { type: "GET_RECIPES_START" | "CREATE_RECIPE_START" }
   | { type: "GET_RECIPES_SUCCESS", recipes: Recipe[] }
-  | { type: "GET_RECIPES_FAILURE", error: Error };
+  | { type: "CREATE_RECIPE_SUCCESS", recipe: Recipe }
+  | { type: "GET_RECIPES_FAILURE" | "CREATE_RECIPE_FAILURE", error: Error };
 
 const initialState: State = {
   recipes: mockRecipes
@@ -20,6 +21,8 @@ export default function recipesReducer(
   switch (action.type) {
     case "GET_RECIPES_SUCCESS":
       return { ...state, recipes: action.recipes };
+    case "CREATE_RECIPE_SUCCESS":
+      return { ...state, recipes: [...state.recipes, action.recipe] };
     default:
       return state;
   }
