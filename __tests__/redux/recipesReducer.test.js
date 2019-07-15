4494f2857d0339b3306472538a32e565655251a8
tuzmusic <tuzmusic@gmail.com>
2019-07-15 17:40:34 -0400

    add some recipe reducer tests

--- __tests__/redux/recipesReducer.test.js
+++ __tests__/redux/recipesReducer.test.js
@@ -1,5 +1,6 @@
 // @flow
 import recipesReducer from "../../src/redux/reducers/recipesReducer";
+import Recipe from "../../src/models/Recipe";
 
 describe("recipe reducer", () => {
   const initialState = { recipes: [] };
@@ -7,5 +8,19 @@ describe("recipe reducer", () => {
     expect(recipesReducer(initialState, { type: "GET_RECIPES_START" })).toEqual(
       initialState
     );
+  });
+
+  it("add recipes from a successful fetch", () => {
+    const recipes = Array(5).fill(new Recipe());
+    expect(
+      recipesReducer(initialState, { type: "GET_RECIPES_SUCCESS", recipes })
+    ).toEqual({ recipes });
+  });
+
+  it("should add a new recipe", () => {
+    const recipe = new Recipe();
+    expect(
+      recipesReducer(initialState, { type: "CREATE_RECIPE_SUCCESS", recipe })
+    ).toEqual({ recipes: [recipe] });
   });
 });
