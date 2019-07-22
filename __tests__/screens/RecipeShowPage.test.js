7ff6fd7ea761bcf91f7cfa8dfee486a353d47ef4
tuzmusic <tuzmusic@gmail.com>
2019-07-22 10:51:52 -0400

    modify show page to take a single recipe

--- __tests__/screens/RecipeShowPage.test.js
+++ __tests__/screens/RecipeShowPage.test.js
@@ -3,15 +3,26 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import { RecipeShowPage } from "../../src/screens/RecipeShowPage/RecipeShowPage";
 import recipes from "../../src/api-data/recipes";
+import { render, fireEvent, route } from "../../test-utils";
 
 describe("Recipe Show Page", () => {
   const recipe = recipes[0];
-  const wrapper = mount(
-    <RecipeShowPage recipes={recipes} match={{ params: { id: "1" } }} />
-  );
-  it("should show a recipe", () => {
-    expect(wrapper.find("#recipe-title").text()).toEqual(
-      "Chocolate Chip Cookies"
+
+  let container, getByText;
+
+  beforeEach(() => {
+    const component = render(
+      <RecipeShowPage recipe={recipe} match={{ params: { id: "1" } }} />
     );
+    container = component.container;
+    getByText = component.getByText;
+  });
+
+  it("should show a recipe", () => {
+    expect(getByText("Chocolate Chip Cookies")).toBeDefined();
+  });
+
+  it("should show the ingredients", () => {
+    expect(getByText("1 cup of flour")).toBeDefined();
   });
 });
--- src/screens/RecipeShowPage/RecipeShowPage.js
+++ src/screens/RecipeShowPage/RecipeShowPage.js
@@ -4,14 +4,14 @@ import { connect } from "react-redux";
 import CookView from "./CookView/CookView";
 import type Recipe from "../../models/Recipe";
 
-type Props = { recipes: Recipe[], match: Object };
+type Props = { recipe: Recipe[] };
 
 export class RecipeShowPage extends Component<Props> {
   render() {
-    const recipe = this.props.recipes.find(
-      r => r.id === this.props.match.params.id
-    );
-    return <CookView recipe={recipe} />;
+    // const recipe = this.props.recipes.find(
+    //   r => r.id === this.props.match.params.id
+    // );
+    return <CookView recipe={this.props.recipe} />;
   }
 }
 export default connect(({ recipes }) => ({ recipes: recipes.recipes }))(
