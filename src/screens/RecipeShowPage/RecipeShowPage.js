145d6878d0b2a4e62a965ebd96688b79c67a1916
tuzmusic <tuzmusic@gmail.com>
2019-07-22 17:51:05 -0400

    refactor recipeshowpage props to be recipe (singular)

--- src/screens/RecipeShowPage/RecipeShowPage.js
+++ src/screens/RecipeShowPage/RecipeShowPage.js
@@ -4,14 +4,13 @@ import { connect } from "react-redux";
 import CookView from "./CookView/CookView";
 import type Recipe from "../../models/Recipe";
 
-type Props = { recipe: Recipe[] };
+type Props = { recipe: Recipe };
 
 export class RecipeShowPage extends Component<Props> {
   render() {
     return <CookView recipe={this.props.recipe} />;
   }
 }
-export default connect(
-  // ({ recipes }) => ({ recipes: recipes.recipes })
-  {}
-)(RecipeShowPage);
+export default connect(({ recipes }, { match }) => () => {
+  return { recipe: recipes.recipes.find(r => r.id === match.params.id) };
+})(RecipeShowPage);
